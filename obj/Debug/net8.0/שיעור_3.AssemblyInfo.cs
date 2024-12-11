76510e9ffd285e97accfcce12093ffd65e943f6c
rutDomb <ruti1558@gmail.com>
2024-12-11 21:33:05 +0200

    update

--- obj/Debug/net8.0/שיעור_3.AssemblyInfo.cs
+++ obj/Debug/net8.0/שיעור_3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("שיעור_3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97860a536f1566000f9f83adfd9afd5e47e6c9a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8d5e0b56043389bbf53818b19ef2ed02704d7b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("שיעור_3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("שיעור_3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
